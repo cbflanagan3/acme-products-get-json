d28f79be318393b61b63e55e76cac438a57e4334
Caroline B Flanagan <carolineflan@gmail.com>
2020-06-10 21:37:03 -0400

    acme-companies-json

--- db.js
+++ db.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
 
+
+const readJSON = (file)=> {
+  return new Promise((resolve, reject)=> {
+    fs.readFile(file, (err, data)=> {
+      if(data){
+        try {
+            console.log(resolve(JSON.parse(data.toString())))
+          resolve(JSON.parse(data.toString()));
+        }
+        catch(ex){
+            console.log(ex);
+          reject(ex);
+        }
+      }
+      else {
+          console.log(err);
+        reject(err);
+      }
+    });
+  });
+};
+
+module.exports = {
+  readJSON
+};
--- server.js
+++ server.js
@@ -1,1 +1,46 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
+
+const readJSON = (file)=> {
+  return new Promise((resolve, reject)=> {
+    fs.readFile(file, (err, data)=> {
+      if(data){
+        try {
+          resolve(JSON.parse(data.toString())).then(data => data);
+        }
+        catch(ex){
+          reject(ex);
+        }
+      }
+      else {
+        reject(err);
+      }
+    });
+  });
+};
+
+const PORT = 3000;
+
+const app = express()
+const products = './products.json';
+const companies = './companies.json';
+let prodData = '';
+console.log(prodData);
+
+// app.use(express.static(path.join(__dirname, '../acme-products-get-json')))
+
+app.get('/', (req, res)=> {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.get('/api/products', (req, res) => {
+    readJSON(products).then(data => res.send(data));
+}) 
+
+app.get('/api/companies', (req, res) => {
+    readJSON(companies).then(data => res.send(data));
+}) 
+
+app.listen(PORT, () => {console.log(`server is listening on PORT:${PORT}`)});
